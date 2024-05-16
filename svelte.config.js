d3ebb5a5b3d164d384582c1aaa002aca39c69a04
Chronode <140981510+ChronodeAi@users.noreply.github.com>
2024-05-16 16:09:30 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,17 +8,15 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		// Use the static adapter
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: 'index.html'
-		})
+		}),
 	},
 	onwarn: (warning, handler) => {
-		const { code, _ } = warning;
+		const { code } = warning;
 		if (code === 'css-unused-selector') return;
 
 		handler(warning);
