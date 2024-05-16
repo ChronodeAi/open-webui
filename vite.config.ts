31b3521b12ff960698d967f803ec16b7319811c3
Chronode <140981510+ChronodeAi@users.noreply.github.com>
2024-05-16 16:09:02 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	define: {
-		APP_VERSION: JSON.stringify(process.env.npm_package_version)
-	}
+  plugins: [sveltekit()],
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version)
+  },
+  build: {
+    outDir: 'dist' // Ensure this is set to 'dist'
+  }
 });
